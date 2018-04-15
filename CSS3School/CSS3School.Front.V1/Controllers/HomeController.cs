41f3eb1bbdafbc4c7c344173a14c951acfe60d20
Egor <vitkovskynet@yandex.ru>
2018-04-15 14:48:26 +0300

    initial commit for css3

---
+++ CSS3School/CSS3School.Front.V1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSS3School.Front.V1.Controllers
+{
+	public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
