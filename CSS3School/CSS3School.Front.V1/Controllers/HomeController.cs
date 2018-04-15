5e4589bfd95fac62d9d1f60040b840c12e9dc8a2
zhevak <tanya.zhevak@gmail.com>
2018-04-15 21:46:48 +0300

    Hello page

--- CSS3School/CSS3School.Front.V1/Controllers/HomeController.cs
+++ CSS3School/CSS3School.Front.V1/Controllers/HomeController.cs
@@ -5,8 +5,17 @@ namespace CSS3School.Front.V1.Controllers
 	public class HomeController : Controller
     {
         public IActionResult Index()
+
+        {
+            return View();
+        }
+    
+
+    public IActionResult Hello()
+
         {
             return View();
         }
     }
+
 }
