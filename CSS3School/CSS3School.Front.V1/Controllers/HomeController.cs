13b8ea51a574fa1e8ffd7711d06ef1254ea80c7a
Egor <vitkovskynet@yandex.ru>
2018-04-15 21:55:46 +0300

    format

--- CSS3School/CSS3School.Front.V1/Controllers/HomeController.cs
+++ CSS3School/CSS3School.Front.V1/Controllers/HomeController.cs
@@ -5,17 +5,13 @@ namespace CSS3School.Front.V1.Controllers
 	public class HomeController : Controller
     {
         public IActionResult Index()
-
         {
             return View();
         }
-    
 
-    public IActionResult Hello()
-
+		public IActionResult Hello()
         {
             return View();
         }
     }
-
 }
